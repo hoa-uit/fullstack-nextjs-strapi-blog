ef56c2fcfd149f659fd431810f4b5bbb1cd3b66e
Your name <hoan42532@gmail.com>
2022-03-05 21:16:09 +0700

    optimize head tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="vi">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
@@ -22,6 +22,21 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <title>Markintosh</title>
+          <meta charset="UTF-8"></meta>
+          <meta
+            name="description"
+            content="Markintosh's blog là một blog nơi mình chia sẻ những kiến thức và tâm sự cá nhân, rảnh rỗi thì có thể dịch bài "
+          ></meta>
+          <meta
+            name="keywords"
+            content="Markintosh, blog, Markintosh blog, markintosh vercel app, blog của markintosh"
+          ></meta>
+          <meta name="author" content="Markintosh"></meta>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
         </Head>
         <body>
           <Main />
