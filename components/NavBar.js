25e992bfef1ffd0703b2f61b589d8be99967eb5a
Your name <hoan42532@gmail.com>
2022-03-07 17:29:05 +0700

    sliding menu

--- components/NavBar.js
+++ components/NavBar.js
@@ -22,9 +22,11 @@ function NavBar() {
           {/* <FontAwesomeIcon className="icon-crown" icon={faCrown} /> */}
         </Link>
         <div className="effect-underline all-post-nav">
-          <Link href="/posts">
-            <a>Posts</a>
-          </Link>
+          <div className="posts-button">
+            <Link href="/posts">
+              <a>Posts</a>
+            </Link>
+          </div>
         </div>
         <div className="effect-underline pc">
           <Link href="/">
@@ -51,9 +53,37 @@ function NavBar() {
           </a>
         </Link>
       </div>
-      <div className="bars">
-        <FontAwesomeIcon className="icon-bars" icon={faBars} />
-      </div>
+
+      {/* 
+      
+      
+      hamberger button
+      */}
+
+      <input id="hamburger" class="hamburger" type="checkbox" />
+      <label class="hamburger" for="hamburger">
+        <i></i>
+        <text>
+          <close>close</close>
+          <open>menu</open>
+        </text>
+      </label>
+      <section class="drawer-list">
+        <ul>
+          <li>
+            <a href="/posts">All posts</a>
+          </li>
+          <li>
+            <a href="#">Write now</a>
+          </li>
+          <li>
+            <a href="#">Find</a>
+          </li>
+          <li>
+            <a href="#">Light mode</a>
+          </li>
+        </ul>
+      </section>
     </nav>
   );
 }
